1bcbee00a1e67271ed5e5a7d2dad74f028f8dde4
Piet Barber <pb@pietbarber.com>
2025-03-24 15:34:59 -0400

    Remove unnecessary blank line in urls.py, new default home page now works without a django error.

--- members/urls.py
+++ members/urls.py
@@ -6,7 +6,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
