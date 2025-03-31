63e7d3b2d1a7c84eeb6f833329e63b0e434094de
Piet Barber <pb@pietbarber.com>
2025-03-30 21:11:38 -0400

    enhance MemberAdmin with inline MemberBadge management and improve BadgeAdmin search functionality

--- members/admin.py
+++ members/admin.py
@@ -5,19 +5,9 @@ from tinymce.widgets import TinyMCE
 from django.db import models
 from .models import Badge
 from django.utils.html import format_html
+from django.contrib import admin
+from .models import Member, Badge, MemberBadge
 
-
-class BadgeAdmin(admin.ModelAdmin):
-    formfield_overrides = {
-        models.TextField: {'widget': TinyMCE(attrs={'cols': 80, 'rows': 10})},
-    }
-
-admin.site.register(Badge, BadgeAdmin)
-
-@admin.register(Member)
-class MemberAdmin(UserAdmin):
-    model = Member
-    list_display = ("username", "email", "is_staff", "is_active")
 
 
 from django.contrib import admin
@@ -36,3 +26,23 @@ class MemberBadgeAdmin(admin.ModelAdmin):
     list_display = ("member", "badge", "date_awarded")
     list_filter = ("badge",)
     search_fields = ("member__first_name", "member__last_name", "badge__name")
+
+class BadgeAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE(attrs={'cols': 80, 'rows': 10})},
+    }
+    search_fields = ['name', 'description']  
+
+admin.site.register(Badge, BadgeAdmin)
+class MemberBadgeInline(admin.TabularInline):
+    model = MemberBadge
+    extra = 1  # Show one empty row to add a badge
+    autocomplete_fields = ["badge"]  # Optional: if you have lots of badges
+
+class MemberAdmin(admin.ModelAdmin):
+    inlines = [MemberBadgeInline]
+    list_display = ("last_name", "first_name", "email", "membership_status")
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("membership_status", "instructor", "towpilot")
+
+admin.site.register(Member, MemberAdmin)
