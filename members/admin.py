44cd7f31df97e5e0037337887131bff686a06504
Piet Barber <pb@pietbarber.com>
2025-03-31 12:04:42 -0400

    no longer store unhashed passwords in DB.  Atrocity!

--- members/admin.py
+++ members/admin.py
@@ -39,10 +39,61 @@ class MemberBadgeInline(admin.TabularInline):
     extra = 1  # Show one empty row to add a badge
     autocomplete_fields = ["badge"]  # Optional: if you have lots of badges
 
-class MemberAdmin(admin.ModelAdmin):
+
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import Member, MemberBadge
+
+class CustomMemberCreationForm(UserCreationForm):
+    class Meta:
+        model = Member
+        fields = ("username", "email", "first_name", "last_name")
+
+class CustomMemberChangeForm(UserChangeForm):
+    class Meta:
+        model = Member
+        fields = ("username", "email", "first_name", "last_name", "membership_status", "instructor", "towpilot")
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib import admin
+from .models import Member, MemberBadge
+
+class CustomMemberCreationForm(UserCreationForm):
+    class Meta:
+        model = Member
+        fields = ("username", "email", "first_name", "last_name")
+
+class CustomMemberChangeForm(UserChangeForm):
+    class Meta:
+        model = Member
+        fields = ("username", "email", "first_name", "last_name", "membership_status", "instructor", "towpilot")
+
+class MemberBadgeInline(admin.TabularInline):
+    model = MemberBadge
+    extra = 0
+
+@admin.register(Member)
+class MemberAdmin(UserAdmin):
+    add_form = CustomMemberCreationForm
+    form = CustomMemberChangeForm
+    model = Member
     inlines = [MemberBadgeInline]
+
     list_display = ("last_name", "first_name", "email", "membership_status")
     search_fields = ("first_name", "last_name", "email")
     list_filter = ("membership_status", "instructor", "towpilot")
 
-admin.site.register(Member, MemberAdmin)
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Membership", {"fields": ("membership_status", "instructor", "towpilot")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "first_name", "last_name", "password1", "password2"),
+        }),
+    )
