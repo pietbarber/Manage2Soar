e46c454f771951d7de855e8cdfa0a9a5e93c7a63
Piet Barber <pb@pietbarber.com>
2025-04-04 23:08:59 -0400

    Remove unused import of login_required decorator from views.py

--- members/views.py
+++ members/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.urls import reverse
 from .models import Member, Biography
