33420668dbd52a72fd7f753600df80137463e703
Piet Barber <pb@pietbarber.com>
2025-04-11 19:54:50 -0400

    Add grouped syllabus overview template and view with corresponding URL

--- instructors/urls.py
+++ instructors/urls.py
@@ -6,6 +6,7 @@ app_name = "instructors"
 urlpatterns = [
     path("", views.instructors_home, name="index"),
     path("syllabus/", views.syllabus_overview, name="syllabus_overview"),
+    path("syllabus/grouped/", views.syllabus_overview_grouped, name="syllabus_grouped"),
 
 ]
 
--- instructors/views.py
+++ instructors/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .decorators import instructor_required
-from .models import TrainingLesson
+from .models import TrainingLesson, SyllabusDocument, TrainingPhase
 
 # instructors/views.py
 @instructor_required
@@ -12,3 +12,15 @@ def instructors_home(request):
 def syllabus_overview(request):
     lessons = TrainingLesson.objects.all().order_by("code")
     return render(request, "instructors/syllabus_overview.html", {"lessons": lessons})
+
+@instructor_required
+def syllabus_overview_grouped(request):
+    phases = TrainingPhase.objects.prefetch_related("lessons").all()
+    header = SyllabusDocument.objects.filter(slug="header").first()
+    materials = SyllabusDocument.objects.filter(slug="materials").first()
+
+    return render(request, "instructors/syllabus_grouped.html", {
+        "phases": phases,
+        "header": header,
+        "materials": materials,
+    })
