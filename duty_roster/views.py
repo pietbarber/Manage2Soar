684855fc67c863d5e74d9922faab964985378a27
Piet Barber <pb@pietbarber.com>
2025-04-17 20:53:16 -0400

    Initial (still broken) duty_roster section

--- duty_roster/views.py
+++ duty_roster/views.py
@@ -1,8 +1,3 @@
 from django.shortcuts import render
-from members.decorators import active_member_required
 
-
-
-@active_member_required
-def index(request):
-    return render(request, "duty_roster/index.html")
+# Create your views here.
