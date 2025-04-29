a6b6e0ea86651b9316a62e5025f7f10defe10deb
Piet Barber <pb@pietbarber.com>
2025-04-28 20:16:33 -0400

    Add public syllabus overview and detail views for non-instructor access

--- instructors/views.py
+++ instructors/views.py
@@ -24,6 +24,27 @@ from members.models import Member
 from members.constants.membership import DEFAULT_ACTIVE_STATUSES
 
 
+# Training Syllabus links available to the public, no instructor login required. 
+
+def public_syllabus_overview(request):
+    phases = TrainingPhase.objects.prefetch_related("lessons").all()
+    header = SyllabusDocument.objects.filter(slug="header").first()
+    materials = SyllabusDocument.objects.filter(slug="materials").first()
+
+    return render(request, "instructors/syllabus_grouped.html", {
+        "phases": phases,
+        "header": header,
+        "materials": materials,
+        "public": True,
+    })
+
+def public_syllabus_detail(request, code):
+    lesson = get_object_or_404(TrainingLesson, code=code)
+    return render(request, "instructors/syllabus_detail.html", {
+        "lesson": lesson,
+        "public": True,
+    })
+
 
 
 
@@ -37,6 +58,7 @@ def instructors_home(request):
 def syllabus_overview(request):
     lessons = TrainingLesson.objects.all().order_by("code")
     return render(request, "instructors/syllabus_overview.html", {"lessons": lessons})
+
 
 @instructor_required
 def syllabus_overview_grouped(request):
