14cf8c374de7f2aea729f9d8c1aae87a0a3dc599
Piet Barber <pb@pietbarber.com>
2025-09-04 21:47:46 -0400

    Add instructors training phases data and update load order in script

--- loaddata/loaddata.sh
+++ loaddata/loaddata.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ./manage.py loaddata loaddata/instructors.ClubQualificationType.json
-./manage.py loaddata loaddata/instructors.SyllabusDocument.json
+./manage.py loaddata loaddata/instructors.TrainingPhase.json
 ./manage.py loaddata loaddata/instructors.TrainingLesson.json
+./manage.py loaddata loaddata/instructors.SyllabusDocument.json
 tar xvfz loaddata/icons.tar.gz 
