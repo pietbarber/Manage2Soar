d543b0821caac06f3421203292bea568f8cb84ca
Piet Barber <pb@pietbarber.com>
2025-10-15 23:54:03 -0400

    Enhance CMS admin interface with helper messages and improve PDF document display

--- cms/admin.py
+++ cms/admin.py
@@ -12,18 +12,36 @@ class DocumentInline(admin.TabularInline):
 
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
+    def changelist_view(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['admin_helper_message'] = self.admin_helper_message
+        return super().changelist_view(request, extra_context=extra_context)
     list_display = ("title", "slug", "parent", "is_public", "updated_at")
     search_fields = ("title", "slug")
     list_filter = ("is_public", "parent")
     prepopulated_fields = {"slug": ("title",)}
     inlines = [DocumentInline]
 
+    admin_helper_message = (
+        "<b>CMS Pages:</b> Use this to create arbitrary pages and directories under /cms/. Attach documents below. "
+        "Leave 'Parent' blank for top-level pages."
+    )
+
 
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
+    def changelist_view(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['admin_helper_message'] = self.admin_helper_message
+        return super().changelist_view(request, extra_context=extra_context)
     list_display = ("title", "file", "page", "uploaded_by", "uploaded_at")
     search_fields = ("title", "file")
     list_filter = ("page",)
+
+    admin_helper_message = (
+        "<b>CMS Documents:</b> These are files (PDFs, images, etc.) attached to CMS Pages. "
+        "To add a document to a page, use the inline form on the CMS Page itself."
+    )
 
 
 class HomePageImageInline(admin.TabularInline):
@@ -34,13 +52,31 @@ class HomePageImageInline(admin.TabularInline):
 
 @admin.register(HomePageContent)
 class HomePageContentAdmin(admin.ModelAdmin):
+    def changelist_view(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['admin_helper_message'] = self.admin_helper_message
+        return super().changelist_view(request, extra_context=extra_context)
     list_display = ("title", "updated_at")
     search_fields = ("title",)
     inlines = [HomePageImageInline]
 
+    admin_helper_message = (
+        "<b>CMS Page Content:</b> Use this to edit the homepage or member homepage content. "
+        "This is not used for arbitrary CMS pages under /cms/."
+    )
+
 
 @admin.register(HomePageImage)
 class HomePageImageAdmin(admin.ModelAdmin):
+    admin_helper_message = (
+        "<b>CMS Page Images:</b> These images are attached to homepage or member homepage content. "
+        "Use this to manage images for those special pages."
+    )
+
+    def changelist_view(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['admin_helper_message'] = self.admin_helper_message
+        return super().changelist_view(request, extra_context=extra_context)
     list_display = ("page", "caption", "order")
     list_filter = ("page",)
 
