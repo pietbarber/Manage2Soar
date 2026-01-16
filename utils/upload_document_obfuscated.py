b444bcbd41134680613e036b8bfebad6712b9181
Piet Barber <pb@pietbarber.com>
2026-01-15 21:32:47 -0500

    Simplify filename sanitization fallback

--- utils/upload_document_obfuscated.py
+++ utils/upload_document_obfuscated.py
@@ -1,7 +1,6 @@
 import os
 import secrets
 
-from django.core.exceptions import SuspiciousFileOperation
 from django.utils.text import get_valid_filename
 
 
@@ -10,9 +9,10 @@ def upload_document_obfuscated(instance, filename):
     page_slug = instance.page.slug if instance.page else "uncategorized"
     base_filename = os.path.basename(filename)
     name, ext = os.path.splitext(base_filename)
-    try:
-        safe_name = get_valid_filename(name) or "file"
-    except SuspiciousFileOperation:
+    stripped_name = name.strip()
+    if stripped_name in {"", ".", ".."}:
         safe_name = "file"
+    else:
+        safe_name = get_valid_filename(stripped_name) or "file"
     token = secrets.token_urlsafe(8)
     return f"cms/{page_slug}/{safe_name}-{token}{ext}"
